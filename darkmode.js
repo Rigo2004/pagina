35bbe206cab82fd5a797a31482d1067210126b95
rigoberto <116676971+Rigo2004@users.noreply.github.com>
2023-02-26 21:24:31 -0600

    Add files via upload

--- darkmode.js
+++ darkmode.js
@@ -1,7 +1,6 @@
 
-
 const btn_darkmode = document.getElementById('darkmode');
-btn_darkmode.addEventListener('click', function(){
+btn_darkmode.addEventListener('click', function () {
     /*
     if( document.body.classList.contains('dark') ){
         document.body.classList.remove('dark');
